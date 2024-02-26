a19db462a9468eaa27607c997a179cc1534c51d9
kyldex <raphael.giagnorio@gmail.com>
2024-02-27 00:05:07 +0100

    build style url

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -1,10 +1,11 @@
 import { FC, useEffect, useRef } from 'react';
-import { Map as MaplibreMap } from 'maplibre-gl';
+import { Map as MaplibreMap, NavigationControl } from 'maplibre-gl';
 
 import './Map.scss';
 
 import {
-  MAP_STYLE_URL,
+  MAP_BASE_URL,
+  MAP_ID,
   PARIS_CENTER_COORDS,
   INIT_MAP_ZOOM
 } from '../utils/constants';
@@ -19,10 +20,13 @@ const Map: FC = () => {
     if (mapContainer.current) {
       const maplibreMap = new MaplibreMap({
         container: mapContainer.current,
-        // create a MapTiler account
-        style: `${MAP_STYLE_URL}?key=${MAP_API_KEY}`,
+        style: `${MAP_BASE_URL}/maps/${MAP_ID}/style.json?key=${MAP_API_KEY}`,
         center: PARIS_CENTER_COORDS,
         zoom: INIT_MAP_ZOOM
+      });
+      maplibreMap.addControl(new NavigationControl());
+      maplibreMap.on('load', () => {
+        console.log(maplibreMap.getStyle());
       });
     }
   }, []);
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
 import { LngLat } from '../types/LngLat';
 
-export const MAP_STYLE_URL = 'https://api.maptiler.com/maps/dataviz/style.json';
+export const MAP_BASE_URL = 'https://api.maptiler.com';
+export const MAP_ID = '687c7dc1-8e2a-47d7-85b0-ea731f039b7e';
 export const PARIS_CENTER_COORDS: LngLat = [2.352222, 48.856613];
 export const INIT_MAP_ZOOM = 12;
